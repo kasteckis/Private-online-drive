0a072081d583cf3e1bee8f73ddcd7402ea55b582
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2019-02-27 21:02:19 +0200

    mariaus dizainas, valentino apjungimas su backu,
    BUG: Sign In mygtukas po apjungimo pasikeite, nes keiciau is <input> i <button>

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 session_start();
 require 'includes/mysql_connection.php';
 require 'includes/config.php';
@@ -18,11 +18,13 @@ foreach(glob("backend/*.php") as $back)
 	echo "<title>".$WebsiteTitle."</title>" // Globalus kintamasis keičiamas per includes/config.php
 ?>
 <link rel="icon" href="favicon.png" type="image/png" sizes="16x16"> <!-- Vėliau sukursim tą favicon, gal necrashins -->
+
+<link rel="stylesheet" href="css/style.css">
+
 </head>
 
 <body>
 <?php
-	//TODO: Login sistema
 	//Users : id, nick, status(admin/user) ,password (hashed), suspended (1/0), lastLogged
 
 
@@ -33,31 +35,70 @@ foreach(glob("backend/*.php") as $back)
 
 
 	//debuginimo tikslams
-	echo "Sesijos duomenys<br>";
+	echo "<font color='red'>Sesijos duomenys<br>";
 	echo "id: ".$_SESSION['id']."<br>";
 	echo "nick: ".$_SESSION['nick']."<br>";
 	echo "status: ".$_SESSION['status']."<br>";
 	echo "password: ".$_SESSION['password']."<br>";
 	echo "suspended: ".$_SESSION['suspended']."<br>";
-	echo "lastLogged: ".$_SESSION['lastLogged']."<br>";
+	echo "lastLogged: ".$_SESSION['lastLogged']."</font><br>";
 
-	echo '<form method="POST" id="loginFormt">';
+
+/* Mano senas LOGIN (white/black)
+	echo '<form method="POST" id="loginForm">';
 	echo '<input type="text" name="username" placeholder="Username"></input><br>';
 	echo '<input type="password" name="password" placeholder="Password"></input><br>';
 	echo '<button id="btn" name="submit">Login</button><br>';
+	echo '</form>';
+*/
+
+	/*MARIAUS SENAS HTML
+
+	<div class="login-box">
+  <h1>Login</h1>
+  <div class="textbox">
+    <i class="fas fa-user"></i>
+    <input type="text" placeholder="Username">
+  </div>
+
+  <div class="textbox">
+    <i class="fas fa-lock"></i>
+    <input type="password" placeholder="Password">
+  </div>
+
+  <input type="button" class="btn" value="Sign in">
+</div>*/
+
+
+	echo '<form method="POST" id="loginForm">';
+	echo '<div class="login-box">';
+	echo ' <h1>Login</h1>';
+	echo '<div class="textbox">';
+	echo ' <i class="fas fa-user"></i>';
+	echo '<input type="text" name="username" placeholder="Username"></input>';
+	echo '  </div>';
+	echo '  <div class="textbox">';
+	echo '    <i class="fas fa-lock"></i>';
+
+	echo '<input type="password" name="password" placeholder="Password"></input>';
+	echo '  </div>';
+	echo '<button id="btn" name="submit">Sign In</button><br>';
+	echo '</div>';
 	echo '</form>';
 
 	//Kodas vykdomas po LOGIN paspaudimo
 
 	if(isset($_POST['submit']))
 	{
+		echo "test";
 		echo LoginMe($_POST['username'], $_POST['password']);
 	}
 
 
 ?>
 
-</body>
 
+
+</body>
 
 </html>
