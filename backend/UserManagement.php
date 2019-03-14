0f25d16c2ed639c6cd664fd8825c78b0b286384f
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2019-03-14 18:02:33 +0200

    veikia validacijos kuriant useri

--- backend/UserManagement.php
+++ backend/UserManagement.php
@@ -13,8 +13,17 @@
 
 	function CheckForValidation($nick, $status, $password, $hashedPassword, $suspended, $lastLogged)
 	{
+		require 'includes/mysql_connection.php';
+		require 'includes/config.php';
+
 		$validationErrors = array();
 
+		$sqlGetConflictedUsers = "SELECT * FROM Users WHERE nick='$nick'";
+		$resultsGetConflictedUsers = mysqli_query($conn, $sqlGetConflictedUsers);
+	    while($row = mysqli_fetch_assoc($resultsGetConflictedUsers)) 
+	    {
+	    	array_push($validationErrors, "Nickname already exists");
+	    }
 
 
 		if($nick == null)
