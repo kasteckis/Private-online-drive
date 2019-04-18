55a737d1c37d13ad4bf20e45f33c4cca0c80009b
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2019-04-18 20:10:36 +0300

    istryniau su react susijusius failus

--- src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default {
-  user: {
-    login: (credentials) => axios
-    .post('http://localhost:8080/react-app/backend/LoginHandler.php', { credentials })
-    .then(res => res.data.username)
-}
-}
--- src/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import user from "./reducers/user";
-
-export default combineReducers({
-  user
-});
