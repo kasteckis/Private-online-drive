33c5c454d856efe245649b930274fb7e8bdb1106
Tidish <tidish@inbox.lt>
2019-04-27 22:45:01 +0300

    View files design

--- viewfiles.php
+++ viewfiles.php
@@ -15,8 +15,9 @@ foreach(glob("backend/*.php") as $back)
 <head>
 <meta charset="UTF-8">
 <title><?php echo $WebsiteTitle; ?></title>
-<link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16" /> 
-<!--<link rel="stylesheet" href="css/style.css"> -->
+<link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16" />
+<link rel="stylesheet" href="css/styleSettings.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 
 </head>
 
@@ -25,11 +26,17 @@ foreach(glob("backend/*.php") as $back)
 
 	if($_SESSION['status'] == "admin")
 	{
+    ?>
+    <div class="background">
+			<div class="back">
+        <?php
 		//Mygtukas atgal
 		echo '<form action="/usermanager">';
 	    echo '<input type="submit" value="Back" />';
 		echo '</form><br><br>';
-
+    ?>
+    </div>
+    <?php
 		// apsaugos jeigu useris cia pateko be sesijos reiksmes
 		if($_SESSION['viewingFiles'] == null)
 		{
@@ -39,28 +46,32 @@ foreach(glob("backend/*.php") as $back)
 
 		$usersDirectory = "./files/".$_SESSION['viewingFiles'];
 		$fileList = scandir($usersDirectory);
+    echo "<h2>Viewing <b>".$_SESSION['viewingFiles']."</b> files!</h2>";
+    echo '<div class="changebox">';
 
-		echo "Viewing <b>".$_SESSION['viewingFiles']."</b> files!<br>";
 		echo "<form method='POST'>";
 		$thereAreNoFiles = true;
 		foreach ($fileList as $key => $value)
 		{
 			if($value == "." || $value == "..")
-				continue; 
+				continue;
 
-			
-			echo "<input type='checkbox' name='selectedItemsToDelete[]' value='".$value."'>
-			<a href='./files/".$_SESSION['viewingFiles']."/".$value."'>".$value."</a><br>";
+        echo '<div class="file-list">';
+          echo "<i class='far fa-file'></i>
+    			<a href='./files/".$_SESSION['viewingFiles']."/".$value."'>".$value."</a>
+          <input type='checkbox' class='list-check' name='selectedItemsToDelete[]' value='".$value."'>";
+        echo '</div>';
+
 
 			$thereAreNoFiles = false;
 		}
 
 		//Jeigu nera failu direktorijoja, nerodys delete mygtuko
-		
+
 		if(!$thereAreNoFiles)
 		{
-			echo "<button type='submit' name='delete'>Delete selected</button><br>";
-		} 
+			echo "<button type='submit' class='delete-button' name='delete'>Delete selected</button><br>";
+		}
 		echo "</form>";
 
 		if(isset($_POST['delete']))
@@ -85,15 +96,15 @@ foreach(glob("backend/*.php") as $back)
 			echo "<font color='red'>This user has no files in his directory!</font><br>";
 
 		echo "<br><br>";
-
+    echo '</div>';
 	}
 	else
 	{
 		echo '<meta http-equiv="refresh" content="0; url=./errorAuthorization.shtml" />';
 		echo "You are not authorised to view this page!<br>";
 	}
-
 ?>
+</div>
 
 
 
