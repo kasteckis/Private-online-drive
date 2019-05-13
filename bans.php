bb8140b78c3566cd2cc52ac15d25b2f0512043c4
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2019-05-13 17:42:21 +0300

    bans.php pilnai veikia

--- bans.php
+++ bans.php
@@ -33,11 +33,59 @@ foreach(glob("backend/*.php") as $back)
 				echo '<form action="/manager">';
 				echo '<input type="submit" value="Back" />';
 				echo '</form>';
-				echo "Coming soon";
+
+			    $sqlGetBans = "SELECT * FROM BadLogins ORDER BY lastLogin DESC";
+			    $resultsGetBans = mysqli_query($conn, $sqlGetBans);
+
+			    if (mysqli_num_rows($resultsGetBans) > 0)
+			    {
+					echo "<form method='POST'>";
+					echo '<table method="POST">';
+					echo '<tr>';
+				    echo '<th>IP</th>';
+				    echo '<th>Tries to login into account</th> ';
+				    echo '<th>Banned till</th>';
+				    echo '<th>Date</th>';
+				    echo '<th>Delete</th>';
+				    echo '</tr>';
+				    while($row = mysqli_fetch_assoc($resultsGetBans))
+				    {
+				    	echo "<tr>";
+				        echo "<td>".$row['ip']."</td>";
+				        echo "<td>".$row['tries']."</td>";
+				        echo "<td>".$row['bannedTill']."</td>";
+				        echo "<td>".$row['lastLogin']."</td>";
+				        echo "<td><button name=".$row['id'].">X</button></td>";
+				        echo "</tr>";
+
+				        if(isset($_POST[$row['id']]))
+				        {
+				        	$sqlDeleteThisBan = "DELETE FROM BadLogins WHERE id=".$row['id'];
+				        	if(mysqli_query($conn, $sqlDeleteThisBan))
+				        	{
+				        		echo "Succesfully unbanned ".$row['ip'];
+				        		header("Refresh:0");
+				        	}
+				        	else
+				        	{
+				        		echo "Critical error bans.php.<br>!"; //neturetu but
+				        	}
+				        }
+				    }
+				    echo "</table>";
+				    echo "</form>";
+				} 
+				else 
+				{
+				    echo "There are no banned IP's!<br>";
+				}
+
+
 				?>
 
 			</div>
 			<?php
+
 	}
 	else
 	{
