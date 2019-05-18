1f43465acf49b04a626a132bd3ca250fe1e0423a
Valentinas Kasteckis <41127259+Tortonas@users.noreply.github.com>
2019-05-18 15:08:00 +0300

    eina issaugoti uzrasus kaip .txt faila

--- notes.php
+++ notes.php
@@ -28,7 +28,9 @@ require 'includes/config.php';
 					echo '<textarea rows="15" cols="50" name="note">';
 					echo $row['note'];
 					echo '</textarea>';
-					echo "<button name='update'>Update</button>";
+					echo "<button name='update'>Update</button><br><br>";
+					echo "<button name='save'>Save note in directory</button>";
+					echo "<input name='fileName' value='note".date("Y-m-d")."' placeholder='Note file name'></input>";
 					echo "</form>";
 					?>
 				</div>
@@ -46,6 +48,17 @@ require 'includes/config.php';
 					{
 						echo "ERROR."; //niekada neturetu buti
 					}
+				}
+
+				if(isset($_POST['save']))
+				{
+					$fileName = mysqli_real_escape_string($conn, $_POST['fileName']);
+					$myfile = fopen("./files/".$_SESSION['nick']."/".$fileName.".txt", "w");
+					$txt = mysqli_real_escape_string($conn, $_POST['note']);
+					fwrite($myfile, $txt);
+					fclose($myfile);
+					echo "File was created in your directory with the name ".$fileName.".txt !<br>";
+
 				}
 				?>
 			</div>
