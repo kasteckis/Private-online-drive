f4a36d5585d0b20cf2c1d69d2e11858e68ea0ac7
Tidish <tidish@inbox.lt>
2019-05-24 13:19:49 +0300

    Added settings to changed pages

--- settings.php
+++ settings.php
@@ -16,7 +16,12 @@ require 'includes/config.php';
 			include 'includes/file-nav.php';
       ?>
 
-			<div class="changebox">
+			<div class="sub-page-main">
+				<div class="display-menu">
+					<!-- Or delete just the button if no buttons on the page -->
+					<button class="btn-display-menu" type='submit' name='dosmth' ><i class="fas fa-trash-alt"></i> button example</button>
+				</div>
+			<div class="main">
 				<?php
 				echo "<h3>Change password:</h3>";
 				echo "<form method='POST'>";?>
@@ -51,8 +56,8 @@ require 'includes/config.php';
 					echo "</form>";
 				?>
 				</div>
-			</div>
 		</div>
+	</div>
 
 		<?php
 		if(isset($_POST['submitChange']))
@@ -113,7 +118,7 @@ require 'includes/config.php';
 ?>
 
 
-
+</div>
 </body>
 
 </html>
